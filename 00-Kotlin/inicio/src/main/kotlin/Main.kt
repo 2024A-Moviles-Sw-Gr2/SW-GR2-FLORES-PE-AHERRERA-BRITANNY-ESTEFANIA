14c790657daffaba563a876af1fd480db136021e
Brit <carlos.landazuri@epn.edu.ec>
2024-05-09 08:52:38 -0500

    kotlin primera clase

---
+++ 00-Kotlin/inicio/src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+import java.util.*
+
+fun main(){
+    println("Hola mundo")
+    val inmutable: String = "Vicente"
+
+    //MUTABLES
+    var mutable: String = "Vicente"
+    mutable = "Adrian"
+    // Duck Typing
+    var ejemploVariable = " Adrian Eguez "
+    var edadEjemplo: Int = 12
+    ejemploVariable.trim()
+
+    var nombre:String = "Adrian"
+    var sueldo:Double =1.2
+    var estadoCivil: Char = 'C'
+    var mayorEdad: Boolean = true
+    // Clases en Java
+    var fechaNacimiento: Date = Date()
+}
