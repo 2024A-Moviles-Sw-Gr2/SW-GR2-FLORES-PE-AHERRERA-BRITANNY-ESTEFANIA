18466ce3d3da0664a8a0fed27b2bc159baff842b
Stefano Landazuri <119013401+CATEVA26@users.noreply.github.com>
2024-05-17 10:41:54 -0500

    Operaciones en arreglos final

--- 00-Kotlin/inicio/src/main/kotlin/Main.kt
+++ 00-Kotlin/inicio/src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import java.util.*
+import kotlin.collections.ArrayList
 
 fun main(){
     println("Hola mundo")
@@ -47,7 +48,80 @@ fun main(){
 // calcularSueldo (sueldo, tasa, bonoEspecial)
     calcularSueldo(10.00, bonoEspecial = 20.00)
     calcularSueldo(bonoEspecial = 20.00, sueldo = 10.00, tasa = 14.00)
+// Uso de clases
+    val sumaUno = Suma(1,1)
+    val sumaDos = Suma(null,1)
+    val sumaTres = Suma(1,null)
+    val sumaCuatro = Suma(0,0)
+    sumaUno.sumar()
+    sumaDos.sumar()
+    sumaTres.sumar()
+    sumaCuatro.sumar()
+    println(Suma.pi)
+    println(Suma.elevarAlCuadrado(2))
+    println(Suma.historialSumas)
 
+    //Arreglos Estaticos
+    val arregloEstatico: Array<Int> = arrayOf<Int>(1,2,3)
+    println(arregloEstatico)
+    val arregloDinamico: ArrayList<Int> = arrayListOf<Int>(
+        1,2,3,4,5,6,7,8,9,10
+            )
+    println(arregloDinamico)
+    arregloDinamico.add(11)
+    arregloDinamico.add(12)
+    print(arregloDinamico)
+
+
+    val respuestaForEach: Unit = arregloDinamico
+        .forEach{valorActual: Int ->
+            println("Valor actual: ${valorActual}");
+        }
+    arregloDinamico.forEach{println("Valor actual (it): ${it}")}
+
+    //MAP: Muta o modifica el arreglo
+    //1) se envia el nuevo valor de la iteracion
+    //2) Nos devuleve un NUEVO ARREGLO
+    //de las iteraciones
+    val respuestaMap: List<Double> = arregloDinamico.map {
+        valorActual:Int ->
+        return@map valorActual.toDouble() + 100.00
+    }
+    println(respuestaMap)
+    val respuestaMapDos = arregloDinamico.map { it+15 }
+    println(respuestaMapDos)
+
+    val respuestaFilter: List<Int> = arregloDinamico
+        .filter { valorActual:Int ->
+            //Expresion o condición
+            val mayoresACinco: Boolean = valorActual> 5
+            return@filter mayoresACinco
+        }
+    val respuestaFilterDos = arregloDinamico.filter { it <=5 }
+    println(respuestaFilter)
+    println(respuestaFilterDos)
+
+    val respuestaAny: Boolean = arregloDinamico
+        .any{ valorActual:Int ->
+            return@any (valorActual>5)
+        }
+    println(respuestaAny)
+    val respuestaAll: Boolean = arregloDinamico
+        .all { valorActual: Int ->
+        return@all (valorActual>5)
+        }
+    println(respuestaAll)
+
+    // Reduce -> Valor acumulado, en kotlin empieza en 0
+    // Valor acumulado = 0
+    //[1,2,3,4,5] -> Acumular sumar valores del arreglo
+    val respuestaReduce : Int = arregloDinamico
+        .reduce{acumulado:Int, valorActual:Int ->
+            return@reduce (acumulado+valorActual)
+        }
+    println(respuestaReduce)
+
+    
 
 }
 
@@ -55,6 +129,7 @@ fun main(){
 fun imprimirNombre (nombre:String): Unit{
     println("Nombre: ${nombre}") // Template Strings
 }
+
 fun calcularSueldo(
     sueldo: Double, // Requerido
     tasa: Double = 12.00, // Opcional (defecto)
@@ -83,7 +158,7 @@ abstract class Numeros(
     init{
         this.numeroUno
         this.numeroDos
-        print("Inicializando")
+        print("Inicializando \n")
     }
 }
 abstract class NumerosJava
@@ -96,7 +171,7 @@ abstract class NumerosJava
     ) {
         this.numeroUno = uno
         this.numeroDos = dos
-        print("Inicializando")
+        print("Inicializando \n")
     }
 }
 
@@ -117,6 +192,27 @@ class Suma(
         this.soyPublicoExplicito
         soyPublicoImplicito
     }
+    constructor( // Primer constructor
+        uno:Int?,
+        dos:Int
+    ):this(
+        if(uno==null) 0 else uno,
+        dos
+    )
+    constructor( // Segundo constructor
+        uno:Int,
+        dos:Int?
+    ):this(
+        uno,
+        if(dos == null) 0 else dos,
+    )
+    constructor( //tercer constructor
+        uno:Int?,
+        dos:Int?
+    ):this(
+        if(uno==null) 0 else uno,
+        if(dos == null) 0 else dos,
+    )
     public fun sumar(): Int {
         val total = numeroUno+numeroDos
         agregarHistorial(total)
